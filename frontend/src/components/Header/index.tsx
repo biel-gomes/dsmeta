601836a68355269dfe5b01331a68dc39cf06d64b
Gabriel Gomes <gabriel.gomes2022@outlook.com>
2022-09-05 18:26:22 -0300

    Correção de espaçamento no Header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>Desenvolvido por
 
-                    <a href="https://github.com/biel-gomes">biel-gomes</a>
+                    <a href="https://github.com/biel-gomes"> biel-gomes</a>
 
                 </p>
             </div>
